11d8a985c89b06f68e7b00f152b86be27bae6857
Emilsone <lawsonezekiel68@gmail.com>
2020-08-14 16:28:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <CountryList/>
+  </div>
+</template>
+
+<script>
+import CountryList from './components/CountryList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CountryList
+  }
+}
+</script>
+
+<style>
+body{
+    background-color: #f9f4ef;
+    font-family: 'Poppins', sans-serif;
+    
+}
+</style>
